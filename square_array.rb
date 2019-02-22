7abdb552452280e1bce0f695be39e92ea8247dbc
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-02-22 04:46:10 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
-def square_array(numbers)
-  new_array = []
-  numbers.each{|element| new_array << element ** 2}
-  new_array
-end
+# def square_array(numbers)
+#   new_array = []
+#   numbers.each{|element| new_array << element ** 2}
+#   new_array
+# end
 
 def square_array(numbers)
   numbers.map{|e| e ** 2}
